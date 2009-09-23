d75f68eeee8c8626eb97945d36261524620eb195
James Royston <purpleposeidon@gmail.com>
2009-09-22 23:21:22 -0700

    Okay, now release!

--- zirsam.py
+++ zirsam.py
@@ -6,7 +6,7 @@ If you want a general understanding of how this program works, look at common.py
 """
 
 TODO = """
-Released under GPLv3. I reserve the right to change this at any time or permit others to use it under other licenses, unless that's illegal, in which case this sentence does not apply.
+The code is released under GPLv3. Much of the contents of the sub-directories were created by others, in some cases, modified by myself. I reserve the right to change this at any time or permit others to use it under other licenses, unless that's illegal, in which case this sentence does not apply.
 Contact: purpleposedon@gmail.com
 
 At some point I'll make this the entry point program.
