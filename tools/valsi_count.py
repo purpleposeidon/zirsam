f9ac422713deb9be25e20d21cf2ba04ae5f43fdc
James Royston <purpleposeidon@gmail.com>
2009-12-27 20:19:39 -0800

    Added a new tool.

---
+++ tools/valsi_count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.0
+#Doesn't take into account si & friends; you could use thaumatology, but then "zo ka" would be treated as 1 word.
+import sys
+sys.path.append('./')
+import morphology
+import tokens
+i = 0
+for _ in morphology.Stream():
+  if not isinstance(_, tokens.IGNORABLE):
+    i += 1
+print(i)
