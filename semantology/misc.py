37711b607a101464e2ebedbf415de7cb595c2aa4
James Royston <purpleposeidon@gmail.com>
2010-02-14 17:14:03 -0800

    fails at BNF optimization & semantology-loading

---
+++ semantology/misc.py
@@ -0,0 +1,6 @@
+
+
+PA_VALS = dict(zip("no pa re ci vo mu xa ze bi so".split(), range(0, 10)))
+SE_VALS = dict(zip("! ? se te ve xe".split(), range(0, 6)))
+class SemanticsException(Exception): pass
+
