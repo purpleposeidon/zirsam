238f88b1f72e9afea3249417ab21d48dea8a74d1
James Royston <purpleposeidon@gmail.com>
2010-08-15 20:23:23 -0700

    err, actually I did. >_>

---
+++ zirsam/special_terminals.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import zirsam.magic_bnf
+
+class any_word:
+    def __repr__(self):
+        return "special_terminals.any_word"
+    def match(self, tracker):
+        try:
+            tracker.valsi[tracker.get_offset()]
+        except:
+            return zirsam.magic_bnf.NoMatch
+        
+        tracker.accept_terminal()
+        tracker.current_valsi += 1
+        return zirsam.magic_bnf.Match
